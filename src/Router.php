96993df4b28ccb72b2e702aa4a1273f30f15a440
RudyMasGoNext <rudy.mas@go-next.be>
2024-06-28 14:38:52 +0200

    In progress Router Execute

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,7 @@
 
 namespace Tigress;
 
+use JetBrains\PhpStorm\NoReturn;
 use Tigress\Core;
 
 /**
@@ -72,8 +73,115 @@ class Router
         $this->routes = $this->Core->Routes->routes;
         $this->body = file_get_contents('php://input');
 
+        $this->processURL();
+        $this->execute();
+    }
+
+    /**
+     * function processURL()
+     * This will process the URL and extract the parameters from it.
+     */
+    private function processURL(): void
+    {
+        $defaultPath = '';
+        $basePath = explode('?', urldecode($_SERVER['REQUEST_URI']));
+        $requestURI = explode('/', rtrim($basePath[0], '/'));
+        $requestURI[0] = strtoupper($_SERVER['REQUEST_METHOD']);
+        $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
+        $sizeofRequestURI = sizeof($requestURI);
+        $sizeofScriptName = sizeof($scriptName);
+        for ($x = 0; $x < $sizeofRequestURI && $x < $sizeofScriptName; $x++) {
+            if (strtolower($requestURI[$x]) == strtolower($scriptName[$x])) {
+                $defaultPath .= '/' . $requestURI[$x];
+                unset($requestURI[$x]);
+            }
+        }
+        $this->default = $defaultPath . $this->default;
+        $this->parameters = array_values($requestURI);
+    }
+
+    private function execute(): void
+    {
+        $this->checkFunctions();
+        if ($this->parameters[0] === 'OPTIONS') {
+            $this->respondOnOptionsRequest(200);
+        }
+        $variables = [];
         print('<pre>');
-        print_r($this->routes);
+        foreach($this->routes as $route) {
+            $testRoute = explode('/', $route->path);
+            if (!(count($this->parameters) == count($testRoute))) {
+                continue;
+            }
+            print_r($testRoute);
+            for ($x = 0; $x < count($testRoute); $x++) {
+                print($this->isItAVariable($testRoute[$x]) ? 'TRUE' : 'FALSE');
+                print(strtolower('TestRoute = ' . $testRoute[$x]) . '<br>');
+                print(strtolower('Parameters = ' . $this->parameters[$x]) . '<br>');
+                if ($this->isItAVariable($testRoute[$x])) {
+                    $key = trim($testRoute[$x], '{}');
+                    $variables[$key] = str_replace('__', '/', $this->parameters[$x]);
+                } elseif (strtolower($testRoute[$x]) != strtolower($this->parameters[$x])) {
+                    break 1;
+                }
+            }
+            print_r($variables);
+        }
         print('</pre>');
+    }
+
+    /**
+     * Checks if a certain function exists on the server, or not.
+     * I needed to add this, because else the router wouldn't work on a NGINX server!
+     */
+    private function checkFunctions(): void
+    {
+        if (!function_exists('apache_request_headers')) {
+            function apache_request_headers(): array
+            {
+                $arh = [];
+                $rx_http = '/\AHTTP_/';
+                foreach ($_SERVER as $key => $val) {
+                    if (preg_match($rx_http, $key)) {
+                        $arh_key = preg_replace($rx_http, '', $key);
+                        $rx_matches = explode('_', $arh_key);
+                        if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
+                            foreach ($rx_matches as $ak_key => $ak_val) {
+                                $rx_matches[$ak_key] = ucfirst($ak_val);
+                            }
+                            $arh_key = implode('-', $rx_matches);
+                        }
+                        $arh[$arh_key] = $val;
+                    }
+                }
+                return ($arh);
+            }
+        }
+    }
+
+    /**
+     * function isItAVariable($input)
+     * Checks if this part of the route is a variable
+     *
+     * @param string $input Part of the route to be tested
+     * @return bool Return TRUE is a variable, FALSE if not
+     */
+    private function isItAVariable(string $input): bool
+    {
+        return preg_match("/^{(.+)}$/", $input);
+    }
+
+    /**
+     * Send confirmation for an OPTIONS request
+     *
+     * @param int $httpResponseCode
+     */
+    #[NoReturn] private function respondOnOptionsRequest(int $httpResponseCode): void
+    {
+        http_response_code($httpResponseCode);
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: *');
+        exit;
     }
 }
